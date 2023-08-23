f9cc676bcd4815c53de85e4f643e04cac2a01d87
keyla <keyyleiva@gmail.com>
2023-08-23 15:49:18 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// code your solution here
+
+console.log(superbowlWin(record))
+function superbowlWin(record){
+    const winRecord = record.find((object) =>{
+   return (object.result === "W")
+})
+console.log(winRecord);
+    if(winRecord){
+        return winRecord.year
+    }
+    else {
+        return undefined
+    }
+}
+console.log(superbowlWin(record))
